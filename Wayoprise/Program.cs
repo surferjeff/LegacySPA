3d8b1b9394a581b1ebdd86becb7dfaeee3e40fa7
Jeffrey Rennie <surferjeff@gmail.com>
2023-09-15 15:13:14 -0700

    Move license to Root. Directory.

--- Wayoprise/Program.cs
+++ Wayoprise/Program.cs
@@ -3,6 +3,15 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+if ((Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "")
+    .ToLower() != "development")
+{
+    var port = Environment.GetEnvironmentVariable("PORT");
+    if (port != null) {
+        builder.WebHost.UseUrls($"http://*:{port}");
+    }
+}
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
